c60b017b08d34180b5d6e6b8a51bcd0d88e9484e
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-08 02:45:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,6 @@ end
 
 create_project_hash
 
-# projects 
-# kickstarter.css("li.project.grid_4").first
-# name 
-# project.css("h2.bbcard_name strong a").text
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
